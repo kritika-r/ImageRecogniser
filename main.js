d30e30b47609eb1ede628804e78fadd88e6fe336
kritika-r <82035781+kritika-r@users.noreply.github.com>
2021-07-11 16:51:21 +0530

    all done!

--- main.js
+++ main.js
@@ -1,24 +1,16 @@
- 
-
-var camera;
 Webcam.set({
-  height: 300,
-  width: 350,
-  image_format: 'png',
-  png_quality: 90
+    height: 300,
+    width: 350,
+    image_format:'png',
+    png_quality: 90
 });
- 
-console.log("ml5 version:",ml5.version);
-camera= document.getElementById("cam");
+var camera= document.getElementById("cam");
 Webcam.attach(camera);
 
-function takeASnapshot(){
-    Webcam.snap(function(data_uri){
-     document.getElementById("result").innerHTML= '<img id="captured_image"src="'+data_uri+'">';
+function clickSnapshot(){
+   Webcam.snap(function(data_uri){
+   document.getElementById("results").innerHTML='<img id="captured_image"src="'+data_uri+'">';
 
-    });
+   });
 }
-var classifier= ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json',modelLoaded);
-function modelLoaded(){
-    console.log("model loaded");
-}
+var classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json",modelLoaded);
